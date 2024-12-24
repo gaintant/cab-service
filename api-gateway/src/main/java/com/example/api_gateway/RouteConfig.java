6d8cb266536960717989d38679fa585adc84a835
kaushik shah <kaushikshah1309@gmail.com>
2024-12-24 22:00:04 +0530

    initial commit

---
+++ api-gateway/src/main/java/com/example/api_gateway/RouteConfig.java
@@ -0,0 +1,22 @@
+package com.example.api_gateway;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // Route for user
+                .route("user", r -> r.path("/user/**")
+                        .uri("lb://USER"))
+                // Route for sample client
+                .route("SAMPLE-CLIENT", r -> r.path("/sample/**")
+                        .uri("lb://SAMPLE-CLIENT"))
+                .build();
+    }
+}
+
---
+++ eureka-server/src/main/java/com/kaushik/shopping/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.kaushik.shopping.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
+
